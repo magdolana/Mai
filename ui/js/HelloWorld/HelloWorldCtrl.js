14e4555bfc9f1229c2a4e3e9a00979ba4f1e629d
magdolana <magdolana@hotmail.com>
2015-02-20 12:26:50 -0500

    Initial Commit

---
+++ ui/js/HelloWorld/HelloWorldCtrl.js
@@ -0,0 +1,10 @@
+define(['angular'], function (angular) {
+    'use strict';
+    return angular.module('app.helloworld.ctrls', []).controller('helloCtrl', [
+        '$scope', function($scope) {
+            $scope.hello = 'Hello World!!';
+        }
+    ]).controller('hello2', [
+
+    ]);
+});
